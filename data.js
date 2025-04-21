4b65039acc01b90998f36ac0562a74713ea05d1c
Techvro-gc <airdropnft1232@gmail.com>
2025-04-21 19:49:06 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = {
+
+mongoLink:'mongodb+srv://zack:3090@cluster0.1lzfmed.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',//database link
+
+bot_token:'7899326294:AAFdih2beZjvK3Lju87uWZ7GL9RdHbFm9cg',
+
+bot_name:'GigabyteAirdrop_Bot',
+
+bot_admin: 6198789968,
+
+channelsList : ['@Gigabyteannouncement','@GigabyteSupportChat','@CaptainOfCryptos','@CaptainOfCryptoTeam',],
+
+channelscheck : ['@Gigabyteannouncement','@GigabyteSupportChat','@CaptainOfCryptos','@CaptainOfCryptoTeam',],
+
+tweet : 'Captofcryptos',
+
+rpc : 'https://rpc-mainnet.maticvigil.com/',
+  
+txnurl : 'https://polygonscan.com/tx/',
+
+chain : 'polygon',
+
+cntrctadrss : '0x1Ed8d6b2665D2D7ad2b581DF0dd528039CbD58d1',
+
+fromAddrss : '0xa0A76d41Cf954546E4bb4595507adF64d8e85A32',
+
+pvtkey : '0x3f18187f5d0e777d415573076a7e71e2222d6808809a39fb8a100317e0cb0cf2',
+
+reffer_bonus:10000,
+
+min_wd:40000,
+
+daily_bonus:10000,
+
+currency:'GB',
+
+payment_channel:'@Payoutworld_1'
+
+}
